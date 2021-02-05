37da297ff7983a39abd6d0ca93d35d15a10ef3ba
AshishKumarSinghSengar <65999078+AshishKumarSinghSengar@users.noreply.github.com>
2021-02-05 21:45:41 +0530

    Background added

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-import First from './FirstPage';
-import About from './AboutPage';
-import Footer from './FooterComponent';
-import Service from './ServicesComponent';
+import First from './firstPage';
 
 class Main extends Component{
     render(){
         return(
             <div>
-                <First />
-                <About />
-                <Service />
-                <Footer />
+                <First/>
             </div>
         );
     }
